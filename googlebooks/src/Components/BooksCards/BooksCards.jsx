96610b71840c9534742a6c2d18107375e1918af2
Kenny Waite <kennywaite@Kennys-MacBook-Pro.local>
2021-12-07 08:28:33 +1100

    pseudo

--- googlebooks/src/Components/BooksCards/BooksCards.jsx
+++ googlebooks/src/Components/BooksCards/BooksCards.jsx
@@ -1,10 +1,10 @@
-// import BooksList from "../../Containers/BooksList";
 import styles from "./BooksCards.module.scss";
 
 export const BooksCards = ({ book, index }) => {
     return (
         <div className={styles.BooksCards}>
-            <p class>{book.volumeInfo.title}</p>
+            {/* each of the key info wanted to display from book search fetch */}
+            <p className={styles.BooksCards}>{book.volumeInfo.title}</p>
             <p>{book.volumeInfo.authors}</p>
             <p>{book.volumeInfo.description}</p>
             <img
--- googlebooks/src/Containers/Dashboard/Dashboard.jsx
+++ googlebooks/src/Containers/Dashboard/Dashboard.jsx
@@ -4,9 +4,13 @@ import { useState, useEffect } from "react";
 import SearchBar from "../../Components/SearchBar";
 
 const Dashboard = () => {
+    // creating empty state variable
     const [books, setBooks] = useState([]);
+
+    // creating state variable for search terms with default of Javascript
     const [search, setSearch] = useState("JavaScript");
 
+    // fetch function to access API and turn it into json data
     const getBooks = async () => {
         const response = await fetch(
             `https://www.googleapis.com/books/v1/volumes?q=${search}`,
@@ -15,10 +19,12 @@ const Dashboard = () => {
         setBooks(data.items);
     };
 
+    // mounts and updates get book whenever there is a change in [search]
     useEffect(() => {
         getBooks();
     }, [search]);
 
+    // initial mount of getBooks
     useEffect(() => {
         getBooks();
     }, []);
@@ -33,7 +39,9 @@ const Dashboard = () => {
             <h2 className={styles.container__header}>
                 Search for Google Books
             </h2>
+            {/* Searchbar is component and is passing down handleSearch state variable */}
             <SearchBar handleSearch={setSearch} />
+            {/* Bookslist is component and is passing down data state variable to be further maniuplated and show data */}
             <BooksList data={books} />
         </div>
     );
